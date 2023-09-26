7e4b1af92fa481f1fb420251913c9c8d20064fc0
Parapau <120369026+Parapau@users.noreply.github.com>
2023-09-26 13:21:27 +0200

    Add files via upload

--- S05T01N01FrancoTodaPau/src/main/java/com/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n01/controllers/SucursalController.java
+++ S05T01N01FrancoTodaPau/src/main/java/com/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n01/controllers/SucursalController.java
@@ -1,8 +1,10 @@
 package com.cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n01.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n01.model.domain.Sucursal;
 import com.cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n01.model.dto.SucursalDTO;
 import com.cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n01.model.services.SucursalService;
 
@@ -26,53 +27,63 @@ public class SucursalController {
 
 
 	@PostMapping("/add")
-	public ResponseEntity<SucursalDTO> add (@RequestParam Integer id,  @RequestParam String nom, @RequestParam String pais){
-		ResponseEntity<SucursalDTO> resposta = null;
+	public ResponseEntity<SucursalDTO> add (@RequestParam String nom, @RequestParam String pais){
+		ResponseEntity<SucursalDTO> resposta;
 					
-		
+		SucursalDTO sucursal = service.save(new SucursalDTO(nom, pais));
 
-
+		resposta = new ResponseEntity<SucursalDTO>(sucursal, HttpStatus.CREATED);
 
 		return resposta;
 	}
 
 	@DeleteMapping("/delete/{id}")
 	public ResponseEntity<Object> delete (@PathVariable("id") Integer id){
-		ResponseEntity<Object> resposta = null;
+		ResponseEntity<Object> resposta;
 
 		service.deleteById(id);
-
-
+		
+		resposta = new ResponseEntity<Object>(HttpStatus.NO_CONTENT);
+		
 		return resposta;
-
 	}
 
 	@PutMapping("/update")
-	public ResponseEntity<Sucursal> update (){
-		ResponseEntity<Sucursal> resposta = null;
-
-
+	public ResponseEntity<SucursalDTO> update (@RequestParam Integer id, @RequestParam String nom, @RequestParam String pais){
+		ResponseEntity<SucursalDTO> resposta;
+		SucursalDTO sucursal;
+		Optional<SucursalDTO> sucursalVella = service.findByid(id);
+		
+		if (sucursalVella.isPresent()) {
+			sucursalVella.get().setNomSucursal(nom);
+			sucursalVella.get().setPaisSucursal(pais);
+			service.save(sucursalVella.get());
+			resposta = new ResponseEntity<SucursalDTO>(sucursalVella.get(), HttpStatus.OK);
+		} else {
+			resposta = new ResponseEntity<SucursalDTO>(HttpStatus.NOT_FOUND);
+		}
 
 
 		return resposta;
 	}
 
 	@GetMapping("/getOne/{id}")
-	public ResponseEntity<Sucursal> getOne (@PathVariable("id") Integer id){
-		ResponseEntity<Sucursal> resposta = null;
-
-
-
+	public ResponseEntity<SucursalDTO> getOne (@PathVariable("id") Integer id){
+		ResponseEntity<SucursalDTO> resposta = null;
+		Optional<SucursalDTO> sucursal = service.findByid(id);
+		
+		if (sucursal.isPresent()) {
+			resposta = new ResponseEntity<SucursalDTO>(sucursal.get(), HttpStatus.OK);
+		} else {
+			resposta = new ResponseEntity<SucursalDTO>(HttpStatus.NOT_FOUND);
+		}
 
 		return resposta;
 	}
 
 	@GetMapping("/getAll")
-	public List<ResponseEntity<Sucursal>> getAll (){
-		List<ResponseEntity<Sucursal>> resposta = null;
-
-
-
+	public ResponseEntity<List<SucursalDTO>> getAll (){
+		ResponseEntity<List<SucursalDTO>> resposta = new ResponseEntity<List<SucursalDTO>>(service.findAll(), HttpStatus.OK);
 
 		return resposta;
 	}
