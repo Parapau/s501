ae2f700569a6418b35b971e0650775d1d5c86ba6
Parapau <120369026+Parapau@users.noreply.github.com>
2023-10-11 13:02:00 +0200

    Add files via upload

--- S05T01N03FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n03/controller/FlorController.java
+++ S05T01N03FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n03/controller/FlorController.java
@@ -1,11 +1,54 @@
 package cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n03.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n03.model.DTO.FlorDTO;
+import cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n03.model.service.FlorService;
+
+
+@RestController
+@RequestMapping("/flor")
 public class FlorController {
 	
-
-	WebClient client = WebClient.create("http://localhost:8080");
+	
+	@Autowired
+	FlorService service;
+	
+	
+	@GetMapping("/lala")
+	public String lala () {
+		return "lalalalalalalal aixo funciona be lalalalalalalal";
+	}
+	
+	
+	@GetMapping("/getOne/{id}")
+	public ResponseEntity<FlorDTO> getOne (@PathVariable("id") long id){
+		ResponseEntity<FlorDTO> resposta = null;
+		System.err.println("locolocolocolocolocodondequiera");
+		Optional<FlorDTO> flor = service.findById(id).getBody();
+		System.err.println("Aixo es el controlador, estic controlant");
+		if (flor.isPresent()) {
+			System.err.println("e");
+			System.out.println(flor.get().toString());
+			resposta = new ResponseEntity<FlorDTO>(flor.get(), HttpStatus.OK);
+			System.err.println("eee");
+		} else {
+			resposta = new ResponseEntity<FlorDTO>(HttpStatus.NOT_FOUND);
+		}
+		
+		System.err.println("iniminmainimo");
+		
+		return resposta;
+	}
 	
 	
 }
--- S05T01N03FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n03/model/service/FlorService.java
+++ S05T01N03FrancoTodaPau/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n03/model/service/FlorService.java
@@ -1,14 +1,156 @@
 package cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n03.model.service;
 
-import org.springframework.web.reactive.function.client.WebClient;
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClient.RequestBodySpec;
+import org.springframework.web.reactive.function.client.WebClient.RequestHeadersSpec;
+import org.springframework.web.reactive.function.client.WebClient.UriSpec;
+
+import cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n03.model.DTO.FlorDTO;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+
+
+@Service
 public class FlorService {
 
 
 	private final WebClient client = WebClient.create("http://localhost:9001");
-	
-	public FlorDTO save(FlorDTO dto) {
-		
+
+
+
+	public ResponseEntity<FlorDTO> add (FlorDTO flor){
+
+
+
+		return null;
 	}
-	
+
+
+
+	public ResponseEntity<FlorDTO> delteteById(Integer id) {
+
+
+
+
+		return null;
+	}
+
+
+	public ResponseEntity<FlorDTO> update(FlorDTO dto) {
+
+
+
+		return null;
+	}
+
+
+	public ResponseEntity<List<FlorDTO>> findAll(){
+
+
+
+		return null;
+	}
+
+
+	public Optional<FlorDTO> findById(long id){
+		ResponseEntity<Optional<FlorDTO>> retorn = null;
+		
+		System.out.println("ei hola que tal");
+		
+		WebClient client = WebClient.create("http://localhost:9001");
+
+		UriSpec<RequestBodySpec> uriSpec = client.method(HttpMethod.GET);
+
+		RequestBodySpec bodySpec = uriSpec.uri("/flor/getOne/" + id);
+
+		RequestHeadersSpec<?> headersSpec = bodySpec.bodyValue("data");
+
+		System.err.println("MIRA AQUI PAU PAAAAAAAAAAAAAAAAAAAAAAAAAAAAAU");
+
+		WebClient.ResponseSpec response = headersSpec.retrieve();
+		
+		System.err.println("BLABLABLABLABALBALBALBALBALBALBALBLABLABLABLAB");
+		
+		ResponseEntity<Optional> resposta = response.toEntity(Optional.class).block(); // TODO Aixo esta malametn 245% pero almenys no peta
+		
+		System.out.println(resposta.getBody().toString());
+		
+		System.err.println("akdnawkdjhakjwdhkajwdhkjawhdkjahwdkjh");
+		
+//		if (resposta.getStatusCode().equals(HttpStatus.OK)) {
+//			
+//			
+//			retorn = new ResponseEntity<Optional<FlorDTO>>(resposta.getBody() , HttpStatus.OK);
+//			
+//		} else if (resposta.getStatusCode().equals(HttpStatus.NOT_FOUND)) {
+//				
+//			retorn = new ResponseEntity<Optional<FlorDTO>>(HttpStatus.NOT_FOUND);
+//			
+//		} else {
+//			System.out.println("Si aico surt per consola el programador es gilipollaaaaAAAAAAAAAaaaaAAAAAs");
+//		}
+		
+		System.err.println("PAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAU");
+		
+		return resposta.getBody();
+	}
+
+
+
+	public ResponseEntity<Object> deleteAll() {
+
+
+
+		return null;
+	}
+
+
+
+
+
+
+
+
+	public Flux<List<FlorDTO>> getTweetsNonBlocking() {
+		
+		Flux<List> llista = WebClient.create().get().uri("/sucursal/getAll").retrieve().bodyToFlux(List.class);
+
+//		tweetFlux.subscribe(tweet -> log.info(tweet.toString()));
+//		log.info("Exiting NON-BLOCKING Controller!");
+//		return tweetFlux;
+		
+		//llista.fi	
+		 return null;
+	}
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
