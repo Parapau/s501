ca78a6147b0cdf2c9a68ea8e620b4247ea4d0cd6
Parapau <120369026+Parapau@users.noreply.github.com>
2023-10-16 17:50:31 +0200

    Delete S05T01N01FrancoTodaPauApplication directory

--- S05T01N01FrancoTodaPauApplication/src/main/java/cat/itacademy/barcelonactiva/FrancoToda/Pau/s05/t01/n02/model/repository/FlorRepository.java
+++
@@ -1,9 +0,0 @@
-package cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n02.model.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import cat.itacademy.barcelonactiva.FrancoToda.Pau.s05.t01.n02.model.domain.Flor;
-
-@Repository
-public interface FlorRepository extends JpaRepository<Flor, Integer> {}
